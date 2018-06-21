f9789ac33404630f826ed06426c7c706dfc230a4
kinya <kiernan920@gmail.com>
2018-06-21 22:40:07 +0100

    first Associate Commit

---
+++ src/Lambdas1.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Animal {
+    private String species;
+    private boolean canHop;
+    private boolean canSwim;
+    public Animal(String speciesName, boolean hopper, boolean swimmer) {
+        species = speciesName;
+        canHop = hopper;
+        canSwim = swimmer;
+    }
+    public boolean canHop() { return canHop; }
+    public boolean canSwim() { return canSwim; }
+    public String toString() { return species; }
+}
+
+interface CheckTrait {
+    boolean test(Animal a);
+}
+
+interface Updater {
+    String updateSpecies(Animal animal);
+}
+
+class CheckIfHopper implements CheckTrait {
+    public boolean test(Animal animal) {
+        return animal.canHop();
+    }
+}
+
+public class Lambdas1 {
+
+    public static void main(String[] args) {
+        List<Animal> animals = createAnimals();
+        print(animals, (Animal animal) -> animal.canHop() );
+        print(animals, (Animal animal) -> { return animal.canSwim(); });
+        print(animals, (a -> {return a.canSwim();}));
+        print(animals, a -> a.canHop());
+        printUpdatedSpecies( animals, (Animal animal) ->  animal.toString().equals("fish") ? animal.toString() + "FISHfish" :  animal.toString()  );
+        Animal animal = new Animal("animal", false, true);
+    }
+
+    private static List<Animal> createAnimals(){
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("fish", false, true));
+        animals.add(new Animal("kangaroo", true, false));
+        animals.add(new Animal("rabbit", true, false));
+        animals.add(new Animal("turtle", false, true));
+        return animals;
+    }
+
+    private static void printUpdatedSpecies(List<Animal> animals, Updater updater) {
+        for (Animal animal : animals) {
+            String updatedSpecies = updater.updateSpecies(animal);
+            System.out.print(updatedSpecies + " ");
+        }
+        System.out.println();
+    }
+
+    private static void print(List<Animal> animals, CheckTrait checker) {
+        for (Animal animal : animals) {
+            if (checker.test(animal))
+                System.out.print(animal + " ");
+        }
+            System.out.println();
+    }
+}
+
