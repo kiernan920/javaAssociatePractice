10efc4f99dbc522104b47e25302834ae616c2ed7
kinya <kiernan920@gmail.com>
2018-12-20 23:40:03 +0000

    Added Streams and more

--- src/kt/java8/MyPredicate.java
+++ src/kt/java8/MyPredicate.java
@@ -1,11 +1,9 @@
 package kt.java8;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import javax.xml.transform.sax.SAXSource;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Stream;
 
 @FunctionalInterface
 public interface MyPredicate {
@@ -39,6 +37,31 @@ class UsePredicates{
     }
 }
 
+///
+
+class lambdaSyntax {
+    public static void main(String[] args) {
+        List<String> al = new ArrayList<>();
+        Predicate<String> predicate = a -> a.isEmpty();
+        al.removeIf(predicate);
+
+        al.removeIf((String a) -> a.isEmpty());
+
+        al.removeIf((a) -> a.isEmpty());
+
+        al.removeIf(a -> {
+            return a.isEmpty();
+        });
+
+        al.removeIf(a -> {
+            System.out.println("TRUE");
+            return a.isEmpty();
+        });
+    }
+}
+
+///
+
 @FunctionalInterface
 interface Hello{
     String sayHello();
@@ -46,18 +69,75 @@ interface Hello{
 
 class AnonymousMethods{
 
+
     public static void main(String[] args) {
+        String effectivelyFinal = "Hello";
         doSomething(new Hello() {
             @Override
             public String sayHello() {
-                return "Hello";
+                return effectivelyFinal;
             }
         });
         doSomething(() -> "Lambda_Hello");
+        //effectivelyFinal = "Not effectivelyFinal";
     }
 
     static void doSomething(Hello hello){
         System.out.println(hello.sayHello());
+    }
+}
+
+///BUILT IN FUNCTIONAL INTERFACES
+//        Supplier<T> 0 T get
+//        Consumer<T> 1 (T) void accept
+//        BiConsumer<T, U> 2 (T, U) void accept
+//        Predicate<T> 1 (T) boolean test
+//        BiPredicate<T, U> 2 (T, U) boolean test
+//        Function<T, R> 1 (T) R apply
+//        BiFunction<T, U, R> 2 (T, U) R apply
+//        UnaryOperator<T> 1 (T) T apply
+//        BinaryOperator<T> 2 (T, T) T apply
+
+class lambdaSupportInList {
+
+    public static void main(String[] args) {
+        Consumer<String> consumer = singleParameter -> System.out.println(singleParameter);
+        List<String> al = new ArrayList<>(Arrays.asList("A", "B", "C"));
+        al.forEach(consumer);
+        Comparator<String> comparator = (a, b) -> b.compareTo(a);
+        al.sort(comparator);
+        System.out.println("SORTED");
+        al.forEach(consumer);
+        System.out.println("REPLACE ALL");
+        UnaryOperator<String> unaryOperator = a -> a.toLowerCase();
+        al.replaceAll(unaryOperator);
+        al.forEach(consumer);
+    }
+}
+
+class Maps{
+    public static void main(String[] args) {
+        Map<String, String> mappy = new HashMap<>();
+        mappy.put("keyA", "valueA");
+        mappy.put("keyB", null);
+        mappy.putIfAbsent("keyA", "someOtherValue");
+        mappy.putIfAbsent("keyB", "someOtherValue");
+        System.out.println(mappy);
+    }
+}
+
+class Maps2{
+    public static void main(String[] args) {
+        BiFunction<String, String, String> mapper = (existingValue, possibleNewValue)
+        -> existingValue.length() > possibleNewValue.length() + 1 ? existingValue: possibleNewValue + 2;
+        Map<String, String> nodeToNeType = new HashMap<>();
+        nodeToNeType.put("Node1", "RadioNode");
+        nodeToNeType.put("Node2", "ERBS");
+        String valueUsedForNode1 = nodeToNeType.merge("Node1", "Radio", mapper);
+        String valueUsedForNode2 = nodeToNeType.merge("Node2", "LongERBS", mapper);
+        System.out.println(valueUsedForNode1);
+        System.out.println(valueUsedForNode2);
+        System.out.println(nodeToNeType);
     }
 }
 
@@ -83,3 +163,45 @@ class MethodReferences{
         Supplier<ArrayList> lambda4 = () -> new ArrayList();
     }
 }
+
+///
+
+class Streams{
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList("Boris", "Elvis", "Dave"));
+        Stream<String> stream = list.stream();
+        stream.filter(a -> a.startsWith("E")).forEach(System.out::println);
+        //stream.forEach(a -> System.out.println("RUNTIME EX STREAM USED UP!"));
+        System.out.println();
+        list.forEach(System.out::println);
+        System.out.println();
+        list.stream().sorted((a,b) -> a.compareTo(b)).forEach(a -> System.out.println(a));
+        System.out.println();
+        list.stream().forEachOrdered(System.out::println);
+    }
+}
+
+class MoreStreams{
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList("Boris", "Elvis", "Dave"));
+        list.stream().map(a -> a.length())
+                     .peek( a ->  System.out.println("peek" + a))
+                     .filter(a -> a > 4)
+                     .forEach(System.out::println);
+    }
+}
+
+class OtherWaysToGenerateStreams{
+    public static void main(String[] args) {
+        Stream<String> empty = Stream.empty();
+        Stream<Integer> singleElement = Stream.of(1);
+        Stream<Integer> fromArray = Stream.of(1, 2, 3);
+        //Infinite Streams
+        Stream<Double> randoms = Stream.generate(Math::random);
+        Stream<Integer> oddNumbers = Stream.iterate(1, n -> n + 2);
+        oddNumbers.filter(a -> a > 10).limit(100).forEach(System.out::println);
+    }
+}
+
